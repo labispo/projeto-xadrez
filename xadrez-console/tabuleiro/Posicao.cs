4b7cabff54c173c696e86ee454e7f8eebb5e1516
Leandro Almeida Bispo <leandroabispo@hotmail.com>
2018-02-01 18:39:30 -0200

    Criando a classe Posicao

---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace tabuleiro {
+    class Posicao {
+
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int linha, int coluna) {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(linha);
+            sb.Append(", ");
+            sb.Append(coluna);
+            return sb.ToString();
+        }
+
+    }
+}
