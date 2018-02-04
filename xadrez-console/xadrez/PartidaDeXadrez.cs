d1481fe9cba8f42da31ba191da6a6ec831039209
Leandro Almeida Bispo <leandroabispo@hotmail.com>
2018-02-04 11:22:48 -0200

    Testes iniciais com movimentos possiveis das pecas

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -19,7 +19,7 @@ namespace xadrez {
 
         public void executaMovimento(Posicao origem, Posicao destino) {
             Peca p = tab.retirarPeca(origem);
-            p.incrementarQtdMovimentos();
+            //p.incrementarQtdMovimentos();
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
         }
