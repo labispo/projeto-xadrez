85930240962879f0130f9c4824d9e8c67e9c1c2d
Leandro Almeida Bispo <leandroabispo@hotmail.com>
2018-02-09 17:14:07 -0200

    Implementando a verificacao de xeque

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -100,7 +100,7 @@ namespace xadrez {
 
         public HashSet<Peca> pecasEmJogo(Cor cor) {
             HashSet<Peca> aux = new HashSet<Peca>();
-            foreach (Peca x in capturadas) {
+            foreach (Peca x in pecas) {
                 if (x.cor == cor) {
                     aux.Add(x);
                 }
